4cf80024d8d932ee7912be7d3818d2285d1a78b4
Q3-113 <simonoswald@outlook.com>
2020-11-20 19:17:27 +0100

    i mean this is basically functional right?

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,102 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 // RawStr is a type provided by rocket to mark unvalidated strings
 use rocket::http::RawStr;
+use rocket::response::Redirect;
+use rocket::Request;
 
-// TODO: global hashmap to store mappings -> we need singleton + mutex
-// https://stackoverflow.com/questions/27791532/how-do-i-create-a-global-mutable-singleton
+use std::collections::HashMap;
+use std::sync::Mutex;
 
-// TODO: validate the urls passed to the adder endpoint
+use lazy_static::lazy_static;
+
+use validator::validate_url;
+
+// TODO: count and map should be stored in state struct
+lazy_static! {
+    static ref COUNT: Mutex<i32> = Mutex::new(0);
+    static ref MAP: Mutex<HashMap<i32, String>> = Mutex::new(HashMap::new());
+}
+
+fn get_count() -> i32 {
+    *COUNT.lock().unwrap()
+}
+
+fn increment_count() {
+    *COUNT.lock().unwrap() += 1;
+}
+
+fn put_mapping(url: &str) {
+    MAP.lock().unwrap().insert(get_count(), url.to_string());
+}
+
+fn get_mapping(key: i32) -> String {
+    match MAP.lock().unwrap().get(&key) {
+        Some(url) => format!["{}", url],
+        None => "/missing".to_string(),
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/add?<a>&<b>")]
-fn adder(a: &RawStr, b: &RawStr) -> String {
-    let a: i32 = a.parse::<i32>().unwrap();
-    let b: i32 = b.parse::<i32>().unwrap();
-    let sum: i32 = a + b;
-    format!["{}", sum]
+    "welcome to my url shortener!"
 }
 
 #[get("/shorten?<url>")]
 fn shortener(url: &RawStr) -> String {
-    format!["{}", url.to_string()]
+    let url = url.to_string();
+    let msg;
+
+    if validate_url(&url) {
+        msg = format!["{} -- {}", get_count(), url.to_string()];
+        put_mapping(&url.to_string());
+        increment_count();
+    } else {
+        msg = "invalid url".to_string();
+    }
+
+    msg
 }
 
-// TODO: lookup function: hash -> url
+#[get("/lookup?<key>")]
+fn lookup(key: &RawStr) -> String {
+    match key.to_string().parse::<i32>() {
+        Ok(key) => get_mapping(key),
+        Err(error) => error.to_string(),
+    }
+}
+
+#[get("/missing")]
+fn missing() -> String {
+    "this mapping doesn't exist 😲".to_string()
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> Redirect {
+    // get key from request withour leading "/"
+    let key = &(req.uri().to_string())[1..];
+    let key_int;
+
+    match key.to_string().parse::<i32>() {
+        Ok(k) => {
+            key_int = k;
+        }
+        Err(_) => {
+            // TODO: handle this error more explicitly
+            return Redirect::to("/");
+        }
+    }
+
+    let url = get_mapping(key_int);
+    Redirect::to(url)
+}
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, adder, shortener])
-    .launch();
+    rocket::ignite()
+        .mount("/", routes![index, shortener, lookup, missing])
+        .register(catchers![not_found])
+        .launch();
 }
